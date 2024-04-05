65ff7085114e790da0e4e5bc69346b7d70b8dcf8
John Sullivan <jpsullivan234@icloud.com>
2024-04-05 10:48:35 -0500

    add main, txt files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Read from text file
+// store in dictionary
+// create a data structure to store the data
+
+
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
