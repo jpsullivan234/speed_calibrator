b6bae3d08460b4294de9c9fe1ac59fcdd9142e50
John Sullivan <jpsullivan234@icloud.com>
2024-04-23 20:16:08 -0500

    added instructions to run in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,3 +10,11 @@ int main(){
     cout<<"Hello World!"<<endl;
     return 0;
 }
+
+// To run:
+// 1. 'g++ main.cpp'
+// 2. './a.out'
+// To push changes to git:
+// 'git add .'
+// 'git commit -m "(explain the commit here)" '
+// git push
